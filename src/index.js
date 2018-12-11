9c507c993e4fd5b7887f75bbd57d47a7cdc10834
Kandahar <said.imran@student.vinci.be>
2018-12-11 17:41:52 +0100

    testing env values

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./style/cover.css";
 import ReactMain from "./react/components/main";
 
 require("dotenv").config();
+//process.env.NODE_ENV == development
 console.log(process.env);
 console.log(process.env.API_URL);
 const rootElem = document.body.querySelector("#root");
