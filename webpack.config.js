cfb5b1cdd2e9253d90ff5d00e239446ae7566e94
imran1007 <said.imran@student.vinci.be>
2018-12-12 11:21:55 +0100

    teest

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const outputDirectory = "dist";
 const projectRoot = path.resolve(__dirname, "..");
-require("dotenv").config();
+
+console.log(require("dotenv").config());
 //process.env.NODE_ENV == development
 console.log(process.env);
 
